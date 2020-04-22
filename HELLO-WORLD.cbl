dbbc7bad028db2940f9b3ce256619c6f277a5e75
qpwoeirut <zhongbrothers@gmail.com>
2020-04-21 21:41:07 -0700

    add hello world program

---
+++ HELLO-WORLD.cbl
@@ -0,0 +1,14 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello World!"
+            STOP RUN.
+       END PROGRAM HELLO-WORLD.
