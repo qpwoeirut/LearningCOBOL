de7e0f5318fbcd1f88e5beb8a1f15eef171bd35f
qpwoeirut <zhongbrothers@gmail.com>
2020-05-25 15:58:53 -0700

    add util to convert yy,mm,dd to days after 12/31/1600

---
+++ DATE-TO-DAYS.cbl
@@ -0,0 +1,29 @@
+      ******************************************************************
+      * Author: Stanley Zhong
+      * Date: 5/21/2020
+      * Purpose: Convert year, month, day to number of days since
+      *          12/31/1600
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DATE-TO-DAYS.
+
+       DATA DIVISION.
+       LINKAGE SECTION.
+           01 LS-YEAR PIC 9(4).
+           01 LS-MONTH PIC 9(2).
+           01 LS-DAY PIC 9(2).
+           01 LS-OUTPUT PIC 9(9).
+       PROCEDURE DIVISION USING LS-YEAR LS-MONTH LS-DAY LS-OUTPUT.
+       MAIN-PROCEDURE.
+           MOVE 0 TO LS-OUTPUT
+           ADD LS-YEAR TO LS-OUTPUT
+           MULTIPLY 100 BY LS-OUTPUT
+           ADD LS-MONTH TO LS-OUTPUT
+           MULTIPLY 100 BY LS-OUTPUT
+           ADD LS-DAY TO LS-OUTPUT
+
+           MOVE FUNCTION INTEGER-OF-DATE(LS-OUTPUT) TO LS-OUTPUT
+
+           GOBACK.
+       END PROGRAM DATE-TO-DAYS.
